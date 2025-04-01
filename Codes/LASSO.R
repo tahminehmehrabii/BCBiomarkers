793eab7d3c804fbe7f742a3d409a9251fdab24df
Tahmineh Mehrabi <TahminehMehrabii@gmail.com>
2025-04-01 13:00:10 +0330

    Update LASSO.R

--- Codes/LASSO.R
+++ Codes/LASSO.R
@@ -1,50 +1,102 @@
 library(data.table)
 library(glmnet)
+library(forcats)
 library(ggplot2)
 library(dplyr)
 
-setwd("C:/Users/MHR/Desktop/PDAC")
+# Set the current working directory to the project path
+setwd("C:/Users/MHR/Desktop/Breast")
 
-# Load and process train data
-trainSet <- fread("corrected_training_set.csv")
-trainGroup <- factor(trainSet$group, labels = c("Normal", "Tumor"))
+#### Load and process train data ####
+#####################################
+trainSet <- as.data.frame(fread("corrected_training_set.csv"))
+
+trainGroup <- trainSet$group
+
+rownames(trainSet) <- trainSet$sample
+
 trainData <- trainSet[,-c(1:4)]
+
+# essentialNodes <- fread("Res/PPI/essentialNodes.txt", header = FALSE)$V1
+
 keyGenes <- fread("essentialNodes.txt", header = FALSE)$V1
-trainData <- trainData[, colnames(trainData) %in% keyGenes, with = FALSE]
-train <- cbind(group = trainGroup, trainData)
 
-# LASSO logistic regression
+trainData <- trainData[,which(colnames(trainData) %in% keyGenes)]
+
+trainGroup <- as.factor(trainGroup)
+
+levels(trainGroup) <- c("Normal", "Tumor")
+
+train <- cbind(group=trainGroup, trainData)
+#####################################
+
+#### LASSO logistic regression ####
+###################################
 set.seed(123)
-x <- model.matrix(group ~ ., train)[,-1]
-y <- as.integer(train$group == "Tumor")
-cv.lasso <- cv.glmnet(x, y, alpha = 1, family = "binomial", type.measure = "deviance", nfolds = 10)
 
-png("Lambda_BinomialDeviance.png", width = 1800, height = 1800, res = 300)
-plot(cv.lasso, cex.lab = 1.4, cex.axis = 1.2)
+x <- model.matrix(group ~ ., train)[,-1]
+y <- ifelse(train$group == "Tumor", 1, 0)
+
+cv.lasso <- cv.glmnet(x, y, 
+                      alpha = 1,
+                      family = "binomial",
+                      type.measure = "deviance",
+                      nfolds = 10)
+
+png(filename = "Lambda_BinomialDeviance.png",
+    width = 1800, height = 1800, res = 300)
+plot(cv.lasso, axes=T, cex.lab = 1.4, cex.axis = 1.2)
 dev.off()
 
 write.table(cv.lasso$lambda.min, "lambda_min.txt", quote = FALSE, row.names = FALSE, col.names = FALSE)
 
-# Final model and non-zero coefficients
-lasso.model <- glmnet(x, y, alpha = 1, family = "binomial", lambda = cv.lasso$lambda.min)
-coef.nonzero <- coef(lasso.model)[-1,1]
-df.coef.nonzero <- data.frame(Gene = names(coef.nonzero)[coef.nonzero != 0], Coefficient = coef.nonzero[coef.nonzero != 0])
-write.csv(df.coef.nonzero, "coefficients.csv", row.names = FALSE)
+# Final model with lambda.min
+lasso.model <- glmnet(x, as.factor(y),
+                      alpha = 1,
+                      family = "binomial",
+                      lambda = cv.lasso$lambda.min)
 
-write.table(df.coef.nonzero$Gene, "lassoGenes.txt", quote = FALSE, sep = "\t", row.names = FALSE, col.names = FALSE)
 
-# Plot non-zero coefficients
-df.coef.nonzero <- df.coef.nonzero %>% arrange(Coefficient)
-df.coef.nonzero$Gene <- factor(df.coef.nonzero$Gene, levels = df.coef.nonzero$Gene)
+# Non-zero coefficients
+beta <- lasso.model$beta[,1]
+coef.nonzero <- beta[which(beta != 0)]
+df.coef.nonzero <- data.frame(gene=names(coef.nonzero), coefficient=unname(coef.nonzero))
+write.csv(df.coef.nonzero, "coefficients.csv")
+
+beta.nonzero <- names(which(beta != 0))
+
+write.table(x = beta.nonzero,
+            file = "lassoGenes.txt",
+            quote = FALSE,
+            sep = "\t",
+            row.names = FALSE,
+            col.names = FALSE)
+
+gene.coef <- beta[which(beta != 0)]
+dat <- data.frame(Gene=names(gene.coef), Beta=unname(gene.coef))
+dat <- dat[order(dat$Beta),]
+rownames(dat) <- NULL
+
+beta <- as.numeric(union(substr(dat$Beta[1], 1, 6), substr(dat$Beta[c(1:11)], 1, 5)))
+beta <- beta[-2]
 
 png("nonZeroCoefGenes.png", width = 1600, height = 1400, res = 300)
-ggplot(df.coef.nonzero, aes(x = Coefficient, y = Gene, fill = Coefficient > 0)) + 
+
+dat %>% 
+  arrange(Beta) %>% 
+  mutate(Gene = factor(Gene, levels = dat$Gene)) %>% 
+  ggplot(aes(fill = ifelse(Beta > 0, "#D62728", "#1F77B4"), y = Gene, x = Beta)) + 
   geom_bar(stat = "identity", width = 0.65) +
-  geom_text(aes(label = round(Coefficient, 3)), hjust = ifelse(df.coef.nonzero$Coefficient > 0, -0.1, 1.1), size = 3) +
+  geom_text(aes(label = sprintf("%.3f", Beta)), hjust = ifelse(dat$Beta > 0, -0.1, 1.1), 
+            position = position_dodge2(width = 0.8), size = 3) +
   theme_bw() +
-  xlab("Coefficient") + ylab("") +
-  scale_x_continuous(limits = c(min(df.coef.nonzero$Coefficient) - 0.1, max(df.coef.nonzero$Coefficient) + 0.2)) +
+  xlab("Coefficient") +
+  ylab("") +
+  scale_x_continuous(limits = c(-0.5, 2.2)) +
   theme(axis.title.x = element_text(size = 13),
-        axis.text = element_text(size = 10, colour = "black"),
+        axis.text.x = element_text(colour = "black", angle = 0, hjust = 1, size = 10),
+        axis.text.y = element_text(colour = "black", size = 10),
         legend.position = "none")
+
 dev.off()
+
