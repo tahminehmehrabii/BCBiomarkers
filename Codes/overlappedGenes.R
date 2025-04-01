04f8afd37cee9a4b5a0d8a237aabbee59ffe685c
Tahmineh Mehrabi <TahminehMehrabii@gmail.com>
2025-04-01 12:59:27 +0330

    Update overlappedGenes.R

--- Codes/overlappedGenes.R
+++ Codes/overlappedGenes.R
@@ -1,16 +1,34 @@
 library(data.table)
 library(ggvenn)
 
-setwd("C:/Users/MHR/Desktop/PDAC")
+# Set the current working directory to the project path
+setwd("C:/Users/MHR/Desktop/Breast")
 
 degs <- fread("updown.txt", header = FALSE)$V1
 modules <- fread("modulesGenes.txt", header = FALSE)$V1
 
-dir.create("overlappedGenes", showWarnings = FALSE)
+lstDegsModules <- list(
+  DEGs = degs,
+  M1 = modules
+)
 
-png("overlappedGenes/DEGsModules.png", height = 1600, width = 1600, res = 300)
-ggvenn(list(DEGs = degs, M1 = modules), fill_color = c("blue", "red"), fill_alpha = 0.3, 
-       text_size = 6, set_name_size = 5.5, text_color = "black", stroke_alpha = 0.5, stroke_size = 0.2)
+dir.create("overlappedGenes")
+
+
+png("DEGsModules.png", height = 1600, width = 1600, res = 300)
+ggvenn(data = lstDegsModules, 
+       show_elements = FALSE, 
+       show_percentage = FALSE,
+       label_sep = "\n", 
+       fill_color = c("blue", "red"),
+       fill_alpha = 0.3,
+       text_size = 6,
+       set_name_size = 5.5,
+       text_color = "black",
+       stroke_alpha = 0.5,
+       stroke_size = 0.2,
+)
 dev.off()
 
-write.table(intersect(degs, modules), "overlappedGenes/DEGsModules.txt", quote = FALSE, row.names = FALSE, col.names = FALSE)
+DEGsModules <- intersect(degs, modules)
+write.table(DEGsModules, file = "DEGsModules.txt", quote = FALSE, row.names = FALSE, col.names = FALSE)
